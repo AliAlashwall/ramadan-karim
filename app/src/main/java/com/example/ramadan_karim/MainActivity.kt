b42d2b1b3ab13e77525ed97fc3944d7e4176087f
Ali <alialashwall14@gmail.com>
2023-06-30 04:14:53 +0300

    main commit

--- app/src/main/java/com/example/ramadan_karim/MainActivity.kt
+++ app/src/main/java/com/example/ramadan_karim/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.example.ramadan_karim
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.View.GONE
-import android.view.View.INVISIBLE
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.ramadan_karim.databinding.ActivityMainBinding
 import java.io.BufferedReader
 import java.io.InputStreamReader
-
-
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-     val baseFragment = OneFragment()
+    private lateinit var binding: ActivityMainBinding
+     private val baseFragment = OneFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +20,11 @@ class MainActivity : AppCompatActivity() {
         initFragment()
         callBack()
     }
-
     private fun initFragment() {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.add(R.id.oneFragment, baseFragment)
         transaction.commit()
     }
-
     private fun callBack() {
         binding.apply {
             startButton.setOnClickListener {
@@ -56,17 +49,17 @@ class MainActivity : AppCompatActivity() {
         }
         bindDay(DataManager.getCurrentDay())
     }
+    @SuppressLint("SetTextI18n")
     private fun bindDay(day: PrayerTime) {
         binding.apply {
             date.text = day.Date
             Fajr.text = "Fajr ${" ".repeat(50)} ${day.Fajr}"
-            Dhuhr.text = "Dhuhr${" ".repeat(50)} ${day.Dhuhr}"
+            Duher.text = "Duher${" ".repeat(50)} ${day.Duher}"
             Asr.text = "Asr  ${" ".repeat(50)} ${day.Asr}"
             Maghrib.text = "Maghrib${" ".repeat(48)} ${day.Maghrib}"
             Isha.text = "Isha ${" ".repeat(50)} ${day.Isha}"
         }
     }
-
     private fun removeFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.remove(fragment)
