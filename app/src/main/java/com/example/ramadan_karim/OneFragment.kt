d172bed49e538b79b3edb5bf9f366e11a57e011a
Ali <alialashwall14@gmail.com>
2023-06-30 04:15:41 +0300

    fragment commit

---
+++ app/src/main/java/com/example/ramadan_karim/OneFragment.kt
@@ -0,0 +1,21 @@
+package com.example.ramadan_karim
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ramadan_karim.databinding.FragmentOneBinding
+
+class OneFragment : Fragment() {
+    private lateinit var binding : FragmentOneBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentOneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
