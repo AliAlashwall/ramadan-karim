9f630a3fe8634f03e2e99e368292eae42f32c428
Ali <alialashwall14@gmail.com>
2023-07-02 05:04:28 +0300

    Fragment commit

--- app/src/main/java/com/example/ramadan_karim/OneFragment.kt
+++ app/src/main/java/com/example/ramadan_karim/OneFragment.kt
@@ -3,19 +3,27 @@ package com.example.ramadan_karim
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.ramadan_karim.databinding.FragmentOneBinding
 
 class OneFragment : Fragment() {
-    private lateinit var binding : FragmentOneBinding
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    private lateinit var binding: FragmentOneBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentOneBinding.inflate(inflater, container, false)
+
+        callback()
         return binding.root
     }
+
+private fun callback(){
+    val myButton = binding.startButton
+
+    myButton.setOnClickListener {
+        myButton.visibility = GONE
+        parentFragmentManager.beginTransaction().remove(this).commit()
+    }
+}
 
 }
